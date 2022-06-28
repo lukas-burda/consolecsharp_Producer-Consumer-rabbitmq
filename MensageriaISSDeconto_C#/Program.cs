f4735fa58beed834895c14a0d51405bc6b7d16c1
lukas-burda <contatolukasferreira@gmail.com>
2022-06-27 23:34:42 -0300

    simples envio e consumo de mensagem por console do rabbitMq no docker

---
+++ MensageriaISSDeconto_C#/Program.cs
@@ -0,0 +1,88 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace MensageriaISSDeconto_C_
+{
+    class Mensagem
+    {
+        public int id { get; set; }
+        public string body { get; set; }
+        public DateTime createdAt { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            try
+            {
+                using (var connection = factory.CreateConnection())
+                {
+                    using (var channel = connection.CreateModel())
+                    {
+                        var queue = "mensagens";
+                        channel.QueueDeclare(
+                            queue: queue,
+                            durable: false,
+                            exclusive: false,
+                            autoDelete: false,
+                            arguments: null
+                            );
+
+                        string mensagem = JsonConvert.SerializeObject(
+                            new Mensagem()
+                            {
+                                id = 1,
+                                body = "mensagem",
+                                createdAt = DateTime.Now
+                            });
+
+                        Console.WriteLine(mensagem);
+
+                        bool repeat = false;
+                        if (Console.ReadKey().Key.ToString() == "Enter")
+                        {
+                            repeat = true;
+                        }
+                        var cont = 1;
+                        while (repeat)
+                        {
+                            byte[] bytes = Encoding.UTF8.GetBytes(mensagem);
+
+                            channel.BasicPublish(
+                                body: bytes,
+                                routingKey: queue,
+                                basicProperties: null,
+                                exchange: ""
+                                );
+
+                            cont++;
+                            Console.WriteLine("mensagem enviada"+$" {cont}");
+                            
+                            //if (Console.ReadKey().Key.ToString() == "Enter")
+                            //{
+                            //    repeat = true;
+                            //}
+                            //else
+                            //{
+                            //    repeat = false;
+                            //}
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+
+#Baixar a imagem do RabbitMQ: 
+#docker pull rabbitmq:3-management
+
+#Rodar a imagem no docker: 
+##docker run -it --rm --name rabbitMQ -p 8080:15672 -p 5672:5672 -p 25676:25676 rabbitmq:3-management
